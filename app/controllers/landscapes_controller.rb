4582415c76d376bee28ba54efd56e717eeff1f2c
Franck <franck@franck.(none)>
2010-01-18 07:58:29 +0100

    adding access_control to landscapes and players controllers

--- app/controllers/landscapes_controller.rb
+++ app/controllers/landscapes_controller.rb
@@ -1,6 +1,10 @@
 class LandscapesController < ApplicationController  
   layout "admin"
   
+  access_control :debug => true do
+    allow "user"
+  end
+  
   def index
     @landscape = Landscape.first
   end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,10 @@
 class PlayersController < ApplicationController  
   layout "admin"
   
+  access_control :debug => true do
+    allow "user"
+  end
+  
   def index
     @players = Player.all
   end
