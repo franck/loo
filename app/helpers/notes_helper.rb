463293953dfbdb7c49b4217fb36a3ad9e3fcea9b
Franck <franck@franck.(none)>
2010-01-21 09:29:19 +0100

    truncating note in display_map

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,8 +1,12 @@
 module NotesHelper
   
-  def display_note(note)
+  def display_note(note, short=nil)
     html = "<li id='note-#{note.id}'>"
-    html << note.content
+    if short
+      html << truncate(note.content, 200)
+    else
+      html << note.content
+    end
     html << "<div class='note-info'>"
     html << "<span class='player-name'>#{note.user.player_name}</span>, #{l(note.updated_at, :format => :default)}"
     html << "&nbsp;" + link_to("suppr", note_url(note), :class => "note-suppr")
